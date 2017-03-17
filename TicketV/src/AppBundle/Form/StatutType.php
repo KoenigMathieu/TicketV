5a491fad842c485de6d532a151301ea3080095d3
AdriBalla <adriball.dev@gmail.com>
2017-03-17 16:19:17 +0100

    Vues Statut

--- TicketV/src/AppBundle/Form/StatutType.php
+++ TicketV/src/AppBundle/Form/StatutType.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +16,21 @@ class StatutType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('libelle')->add('actif');
+        $builder->add('libelle', TextType::class,['label'=>'Libellé du statut : '])
+                ->add('actif', CheckboxType::class,['label'=>'Disponible : ','required' => false]);
+
+        $builder->get('actif')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($actifAsString) {
+                    // transform the array to a string
+                    return (bool)$actifAsString;
+                },
+                function ($actifAsBoolean) {
+                    // transform the string back to an array
+                    return $actifAsBoolean;
+                }
+            ))
+        ;
     }
     
     /**
