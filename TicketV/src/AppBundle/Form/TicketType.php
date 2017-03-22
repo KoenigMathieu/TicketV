15b19fd49cb24a2d24d4faf348381baadf9a0ce9
AdriBalla <adriball.dev@gmail.com>
2017-03-22 14:35:18 +0100

    vue ticket

--- TicketV/src/AppBundle/Form/TicketType.php
+++ TicketV/src/AppBundle/Form/TicketType.php
@@ -2,7 +2,10 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,33 @@ class TicketType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('libelle')->add('description')->add('idDegreImportance')->add('idProjet')->add('idStatut')->add('tagsTag');
+        $builder->add('libelle',TextType::class,['label'=>'Libellé : '])
+            ->add('description',TextareaType::class,['label'=>'Description : '])
+
+            ->add('idDegreImportance', EntityType::class, array(
+                'class' => 'AppBundle:DegreImportance',
+                'choice_label' => 'libelle',
+                'label'=>'Degré d\'importance : '
+            ))
+
+            ->add('idProjet', EntityType::class, array(
+                'class' => 'AppBundle:Projet',
+                'choice_label' => 'libelle',
+                'label'=>'Projet : '
+            ))
+
+            ->add('idStatut', EntityType::class, array(
+                'class' => 'AppBundle:Statut',
+                'choice_label' => 'libelle',
+                'label'=>'Statut : '
+            ))
+
+            ->add('tagsTag', EntityType::class, array(
+                'class' => 'AppBundle:Tags',
+                'choice_label' => 'libelle',
+                'label'=>'Tags : '
+            ));
+
     }
     
     /**
