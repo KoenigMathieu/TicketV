b1899acf92a8f2665b0a489cb964e333e7e7fa25
AdriBalla <adriball.dev@gmail.com>
2017-03-30 20:23:34 +0200

    page d'accueil

--- TicketV/src/AppBundle/Controller/DefaultController.php
+++ TicketV/src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,23 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+        $em = $this->getDoctrine()->getManager();
+        $id_current_user = $this->getUser()->getId();
+
+        $query = $em->createQuery(
+            'SELECT t FROM AppBundle:Ticket t, AppBundle:SuiviTicket st 
+            WHERE t.idTicket = st.idTicket
+            AND st.idUtilisateur = '.$id_current_user.'
+            AND st.idSuiviTicket = (SELECT MAX(st2.idSuiviTicket) FROM AppBundle:SuiviTicket st2 WHERE st2.idTicket = t.idTicket)'
+        );
+
+        $tickets = $query->getResult();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'tickets' => $tickets
         ]);
     }
 }
